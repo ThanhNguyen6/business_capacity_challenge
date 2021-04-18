d62d3273abd8db577d5fb12be7457b45f2126fcf
Thanh Nguyen <thanhnguyen@Thanhs-MacBook-Pro.local>
2021-04-18 16:02:04 -0700

    home screen realtime change title and current count

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:opun_challenge/core/viewmodels/CRUDModel.dart';
 import 'package:opun_challenge/widgets/number_wheel.dart';
+import '../core/models/businessInfo.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -12,28 +17,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final _minCounter=0;
-  final _maxCounter=30;
-  Color _c = Colors.white;
+  final _counter=0;
+  Color _c = Color.fromARGB(240, 227, 245, 239);
 
-  void _incrementCounter(int inc) {
+  int _incrementCounter(int inc, int _maxCounter, int _counter) {
     int temp = _counter + inc;
     if (_minCounter <= temp && temp< _maxCounter ) {
       setState(() => _counter += inc);
-      _c = Colors.white;
+      _c = Color.fromARGB(240, 227, 245, 239);
+      return temp;
     }
     if (temp == _maxCounter) {
       setState(() {
         _counter += inc;
         _c = Colors.red;
       });
+      return temp;
     }
+    return _counter;
 
   }
 
     @override
     Widget build(BuildContext context) {
+      final crudProvider = Provider.of<CRUDModel>(context);
+      final businessProvider = Provider.of<Business>(context);
+      String name = businessProvider.name ?? widget.title;
+      int cap = businessProvider.capacity ?? 20;
+      int currentCount = businessProvider.count ?? 0;
       return Scaffold(
         backgroundColor: _c,
         body: Stack(
@@ -48,22 +60,44 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   // TODO: remove this counter and show capacity by animating number wheels
                   Text(
-                    'Capacity:',
+                    'Current Count:',
                   ),
-                  Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.headline4,
+                  //Text(
+                  //  '$currentCount',
+                  //  style: Theme.of(context).textTheme.headline4,
+                  //),
+                  Flexible(
+                      child: new StreamBuilder<DocumentSnapshot>(
+                          stream: crudProvider.fetchBusinessDocAsStream('test'),
+                          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (!snapshot.hasData) {
+                              return Text(
+                                '$_counter',
+                                style: Theme.of(context).textTheme.headline4,
+                              );
+                            }
+                            var counter = snapshot.data.data()['count'];
+                            return Text('$counter', style: Theme.of(context).textTheme.headline4);
+                          })
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       IconButton(
                         icon: Icon(FontAwesomeIcons.userMinus),
-                        onPressed: () => _incrementCounter(-1),
+                        onPressed: () async {
+                            int updateCount = _incrementCounter(-1, cap, currentCount);
+                            businessProvider.businessCount(updateCount);
+                            await crudProvider.updateBusiness(Business(name:businessProvider.name, capacity: businessProvider.capacity, count: updateCount), 'test');
+                            },
                       ),
                       IconButton(
                         icon: Icon(FontAwesomeIcons.userPlus),
-                        onPressed: () => _incrementCounter(1),
+                        onPressed: () async {
+                          int updateCount = _incrementCounter(1, cap, currentCount);
+                          businessProvider.businessCount(updateCount);
+                          await crudProvider.updateBusiness(Business(name:businessProvider.name, capacity: businessProvider.capacity, count: updateCount), 'test');
+                        },
                       ),
                     ],
                   ),
@@ -79,8 +113,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          NumberWheel(number: _counter~/10),
-                          NumberWheel(number: _counter%10),
+                          NumberWheel(number: currentCount~/10),
+                          NumberWheel(number: currentCount%10),
                         ],
                       ),
                     ),
@@ -102,9 +136,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     Padding(
                       padding: const EdgeInsets.all(24.0),
                       child:
+                      new StreamBuilder<DocumentSnapshot>(
+                          stream: crudProvider.fetchBusinessDocAsStream('test'),
+                          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (!snapshot.hasData) {
+                              return Text("Capacity Counter");
+                            }
+                            var name = snapshot.data.data()['name'];
+                            return Text('To $name', style: TextStyle(fontSize: 20));
+                          })
                       // TODO: show business name here if it has been saved in the settings page
-                      Text(widget.title,
-                          style: TextStyle(fontSize: 20)),
+                      //Text(name,
+                      //    style: TextStyle(fontSize: 20)),
                     ),
                     IconButton(
                       icon: Icon(Icons.settings),
